b71c5e06ddf2ab810dfa8282b67fdfd3d6251163
Hikvineh <rarifky.aditya@gmail.com>
2019-03-31 06:41:21 +0700

    setup main package

---
+++ go-api/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/hikvineh/go-api/system/app"
+	DB "github.com/hikvineh/go-api/system/db"
+	"github.com/joho/godotenv"
+)
+
+var port string
+
+func init() {
+	flag.StringVar(&port, "port", "8081", "Assigning the port that the server should listen to.")
+
+	flag.Parse()
+	if err := godotenv.Load("config.ini"); err != nil {
+		panic(err)
+	}
+
+	envPort := os.Getenv("PORT")
+	if len(envPort) > 0 {
+		port = envPort
+	}
+}
+
+func main() {
+	// log.Println("Hello World!")
+	db, err := DB.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	s := app.NewServer()
+
+	s.Init(port, db)
+	s.Start()
+}
