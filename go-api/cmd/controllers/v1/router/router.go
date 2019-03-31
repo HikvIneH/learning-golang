ee1674b2a5ebbfd860e2445db1a59a58f020f830
Hikvineh <rarifky.aditya@gmail.com>
2019-04-01 02:41:24 +0700

    add header in middleware

--- go-api/cmd/controllers/v1/router/router.go
+++ go-api/cmd/controllers/v1/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"log"
 	"net/http"
 
 	StatusHandler "github.com/hikvineh/go-api/cmd/controllers/v1/status"
@@ -9,6 +10,15 @@ import (
 
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("X-App-Token")
+		if len(token) < 1 {
+			http.Error(w, "Not Authorized!", http.StatusUnauthorized)
+			return
+		}
+
+		log.Println("Inside Middleware v1")
+		// log.Println(r.Header.Get("X-App-Token"))
+
 		next.ServeHTTP(w, r)
 	})
 }
