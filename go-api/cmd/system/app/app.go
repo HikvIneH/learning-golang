539d1eb5c2bb535242008985bf1aad528cbecfa9
Hikvineh <rarifky.aditya@gmail.com>
2019-04-01 02:41:41 +0700

    add goirlla handler

--- go-api/cmd/system/app/app.go
+++ go-api/cmd/system/app/app.go
@@ -3,10 +3,12 @@ package app
 import (
 	"log"
 	"net/http"
-
-	"github.com/hikvineh/go-api/cmd/system/router"
+	"os"
+	"time"
 
 	"github.com/go-xorm/xorm"
+	"github.com/gorilla/handlers"
+	"github.com/hikvineh/go-api/cmd/system/router"
 )
 
 // Server struct
@@ -35,6 +37,24 @@ func (s *Server) Start() {
 
 	r.Init()
 
-	http.ListenAndServe(s.port, r.Router)
+	handler := handlers.LoggingHandler(os.Stdout, handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Origin", "Cache-Control", "X-App-Token"}),
+		handlers.ExposedHeaders([]string{""}),
+		handlers.MaxAge(1000),
+		handlers.AllowCredentials(),
+	)(r.Router))
+	handler = handlers.RecoveryHandler(handlers.PrintRecoveryStack(true))(handler)
+
+	newServer := &http.Server{
+		Handler:      handler,
+		Addr:         "0.0.0.0" + s.port,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(newServer.ListenAndServe())
+	// http.ListenAndServe(s.port, handler)
 
 }
