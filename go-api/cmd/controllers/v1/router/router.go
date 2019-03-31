b7bc2ffaf2b4c27317a5e167e09d089dc24d963c
Hikvineh <rarifky.aditya@gmail.com>
2019-04-01 02:11:35 +0700

    fix typo

--- go-api/cmd/controllers/v1/router/router.go
+++ go-api/cmd/controllers/v1/router/router.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hikvineh/go-api/pkg/types/routes"
 )
 
-func MiddleWare(next http.Handler) http.Handler {
+func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		next.ServeHTTP(w, r)
 	})
